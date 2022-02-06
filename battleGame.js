908b974cf3647b0d0f37a9c644e6e288de725643
Nell B <nellb28@yahoo.com>
2022-02-06 08:58:45 -0600

    in class exercise change

--- battleGame.js
+++ battleGame.js
@@ -1,10 +1,27 @@
 // 1. Create attack function below.  This will take the following parameters:
 // attackingPlayer, defendingPlayer, baseDamage, variableDamage
+const attack = function(attackingPlayer, defendingPlayer, baseDamage, variableDamage){
+  let totalDamage = baseDamage + getRandomDamage(variableDamage);
+  defendingPlayer.health = defendingPlayer.health - totalDamage;
+  return attackingPlayer.name + ' hits ' + defendingPlayer.name + ' for ' + totalDamage + ' damage!';
+};
 
+function getRandomDamage(maxDamageAmount){
+  return Math.floor(Math.random() * maxDamageAmount);
+}
 
 
 // 2. Create player1 and player2 objects below
 // Each should have a name property of your choosing, and health property equal to 10
+const player1 = {
+  name: 'Elon',
+  health: 10,
+};
+
+const player2 = {
+  name: 'Jeff',
+  health: 10,
+};
 
 
 
